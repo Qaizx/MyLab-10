8bdc32fe2b7bb7c500c5d68fda35349d4acdf963
Qaizx <kaothom@gmail.com>
2021-01-12 14:39:28 +0700

    finish

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,53 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int dice,Student;
+string something;
+
+void roll(){
+    dice = rand() %9 + 1;
+
+}
+
+
+string grade(int x){
+
+    string y = "";
+    if(x == 1)
+        y = "A";
+    else if( x == 2)
+        y = "B+";
+    else if( x == 3)
+        y = 'B';
+    else if( x == 4)
+        y = "C+";
+    else if( x == 5)
+        y = 'C';
+    else if( x == 6)
+        y = "D+";
+    else if( x == 7)
+        y = 'D';
+    else if( x == 8)
+        y = 'F';
+    else if( x == 9)
+        y = 'W';
+        
+    return y;
+
+}
+
+int main(){
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i =0; i<3; i++){
+        cin.get();
+    }
+    roll();
+    Student = dice;
+    something = grade(Student);
+    cout << "You will get " << something << " in this 261102.";
+
+}
+
